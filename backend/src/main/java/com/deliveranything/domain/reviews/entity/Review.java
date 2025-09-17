4eac0ccbb14eb6e4b6a2b050132cf8c140d4b3bc
qkek123 <qkek622378@gmail.com>
2025-09-17 16:02:35 +0900

    feat: builder 추가

--- backend/src/main/java/com/deliveranything/domain/reviews/entity/Review.java
+++ backend/src/main/java/com/deliveranything/domain/reviews/entity/Review.java
@@ -4,10 +4,19 @@ import com.deliveranything.domain.reviews.enums.ReviewTargetType;
 import com.deliveranything.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@Getter
+@Table(name = "review")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review extends BaseEntity {
 
   @Schema(description = "별점")
@@ -44,4 +53,19 @@ public class Review extends BaseEntity {
   @Schema(description = "삭제 상태")
   @Column(name = "is_deleted")
   private boolean isDeleted = false;
+
+  //========================생성 메소드===========================
+  @Builder
+  public Review(
+      int rating,
+      String comment,
+      ReviewTargetType targetType,
+      Long targetId,
+      User user) {
+    this.rating = rating;
+    this.comment = comment;
+    this.targetType = targetType;
+    this.targetId = targetId;
+    this.user = user;
+  }
 }
