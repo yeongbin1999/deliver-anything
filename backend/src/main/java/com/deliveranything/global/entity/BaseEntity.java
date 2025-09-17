f6abd2ca9d2b6fe51120f1ffd5dfd7904aa153af
darancode <darancode@gmail.com>
2025-09-17 15:40:14 +0900

    feat(be): BaseEntity 생성_formatter적용

--- backend/src/main/java/com/deliveranything/global/entity/BaseEntity.java
+++ backend/src/main/java/com/deliveranything/global/entity/BaseEntity.java
@@ -1,11 +1,15 @@
 package com.deliveranything.global.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 // 기본 엔티티 (ID + 생성 시간)
 
@@ -14,11 +18,12 @@ import java.time.LocalDateTime;
 @Getter
 public class BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+  
 }
