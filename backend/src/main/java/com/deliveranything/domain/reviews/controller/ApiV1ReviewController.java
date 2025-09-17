f0c6faeea11b5f079ee4022ade189d04231d8a36
qkek123 <qkek622378@gmail.com>
2025-09-17 21:45:53 +0900

    ReviewService 생성

--- backend/src/main/java/com/deliveranything/domain/reviews/controller/ApiV1ReviewController.java
+++ backend/src/main/java/com/deliveranything/domain/reviews/controller/ApiV1ReviewController.java
@@ -3,8 +3,10 @@ package com.deliveranything.domain.reviews.controller;
 import com.deliveranything.domain.reviews.dto.ReviewCreateRequest;
 import com.deliveranything.domain.reviews.dto.ReviewCreateResponse;
 import com.deliveranything.domain.reviews.entity.Review;
+import com.deliveranything.domain.reviews.service.ReviewService;
 import com.deliveranything.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,13 @@ public class ApiV1ReviewController {
 
   //리뷰 생성
   @PostMapping
-  public ApiResponse<ReviewCreateResponse> review(@RequestBody ReviewCreateRequest request){
-    ReviewCreateResponse response = reviewService.createReview(request, userId);
+  public ApiResponse<ReviewCreateResponse> review(@RequestBody ReviewCreateRequest request,
+      @AuthenticationPrincipal User user
+      //아직 미구현?
+  ) {
+    ReviewCreateResponse response = reviewService.createReview(request
+        //, userId (인증 객체 받아온 후 주석 해제)
+        );
     return ApiResponse.success(response);
   }
 }
---
+++ backend/src/main/java/com/deliveranything/domain/reviews/service/ReviewService.java
@@ -0,0 +1,42 @@
+package com.deliveranything.domain.reviews.service;
+
+import com.deliveranything.domain.reviews.dto.ReviewCreateRequest;
+import com.deliveranything.domain.reviews.dto.ReviewCreateResponse;
+import com.deliveranything.domain.reviews.entity.Review;
+import com.deliveranything.domain.reviews.entity.ReviewPhoto;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+  private final ReviewRepository reviewRepository;
+  private final ReviewPhotoRepository reviewPhotoRepository;
+
+
+  public ReviewCreateResponse createReview(ReviewCreateRequest request) {
+    //리뷰 생성
+    Review review = Review.builder()
+        .targetType(request.targetType())
+        .user() //파라미터로 유저 받아오기
+        .comment(request.comment())
+        .rating(request.rating())
+        .targetId(request.targetId())
+        .build();
+
+    reviewRepository.save(review);
+
+    //리뷰 사진 객체 생성
+    List<ReviewPhoto> reviewPhotos = Arrays.stream(request.photoUrls())
+        .map(url -> ReviewPhoto.builder()
+            .photoUrl(url)
+            .review(review)
+            .build())
+        .toList();
+
+    reviewPhotoRepository.saveAll(reviewPhotos);
+    }
+  }
+}
