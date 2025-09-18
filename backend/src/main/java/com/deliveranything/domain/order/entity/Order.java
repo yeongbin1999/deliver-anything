2665198abeac9432fc8297ec6900de3493e28692
DreamTree <166996451+flffkd12@users.noreply.github.com>
2025-09-18 14:41:42 +0900

    feat(be): 정산 엔티티 생성 (#44)
    * feat(be): 정산 대상 엔티티 생성
    
    * refactor(be): 주문 도메인과 정산 도메인을 분리하도록 연관계 제거
    
    * 추후 확장성 고려 및 결합도 감소
    
    * feat(be): 정산 묶음 처리 엔티티 생성
    
    * 정산 대상 테이블과 연관관계 지정
    
    * refactor(be): Nullable 컬럼과 아닌 것 구분

--- backend/src/main/java/com/deliveranything/domain/order/entity/Order.java
+++ backend/src/main/java/com/deliveranything/domain/order/entity/Order.java
@@ -2,6 +2,7 @@ package com.deliveranything.domain.order.entity;
 
 import com.deliveranything.domain.delivery.entity.Delivery;
 import com.deliveranything.domain.order.enums.OrderStatus;
+import com.deliveranything.domain.user.entity.User;
 import com.deliveranything.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -25,16 +26,12 @@ import lombok.NoArgsConstructor;
 public class Order extends BaseEntity {
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "store_id")
+  @JoinColumn(name = "store_id", nullable = false)
   private Store store;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id")
+  @JoinColumn(name = "user_id", nullable = false)
   private User consumer;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "settlement_id")
-  private Settlement settlement;
 
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "delivery_id", unique = true)
@@ -44,18 +41,22 @@ public class Order extends BaseEntity {
   private List<OrderItem> orderItems = new ArrayList<>();
 
   @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
   private OrderStatus orderStatus;
 
+  @Column(nullable = false)
   private UUID merchantId;
 
+  @Column(nullable = false)
   private String address;
+
   private String riderNote;
   private String storeNote;
 
-  @Column(precision = 19, scale = 2)
+  @Column(nullable = false, precision = 19, scale = 2)
   private BigDecimal totalPrice;
-  @Column(precision = 19, scale = 2)
+  @Column(nullable = false, precision = 19, scale = 2)
   private BigDecimal storePrice;
-  @Column(precision = 19, scale = 2)
+  @Column(nullable = false, precision = 19, scale = 2)
   private BigDecimal deliveryPrice;
 }
--- backend/src/main/java/com/deliveranything/domain/order/entity/OrderItem.java
+++ backend/src/main/java/com/deliveranything/domain/order/entity/OrderItem.java
@@ -22,15 +22,16 @@ public class OrderItem {
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "order_id")
+  @JoinColumn(name = "order_id", nullable = false)
   private Order order;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "product_id")
+  @JoinColumn(name = "product_id", nullable = false)
   private Product product;
 
-  @Column(precision = 19, scale = 2)
+  @Column(nullable = false, precision = 19, scale = 2)
   private BigDecimal price;
 
+  @Column(nullable = false)
   private int quantity;
 }
