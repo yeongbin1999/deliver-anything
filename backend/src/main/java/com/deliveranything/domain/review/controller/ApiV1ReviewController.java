c1d5206189b70f60904ac6c06c28f6f3b39ba7c2
qkek123 <qkek622378@gmail.com>
2025-09-18 12:08:39 +0900

    deleteReview controller 메소드 생성

--- backend/src/main/java/com/deliveranything/domain/review/controller/ApiV1ReviewController.java
+++ backend/src/main/java/com/deliveranything/domain/review/controller/ApiV1ReviewController.java
@@ -5,6 +5,9 @@ import com.deliveranything.domain.review.dto.ReviewCreateResponse;
 import com.deliveranything.domain.review.service.ReviewService;
 import com.deliveranything.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +22,24 @@ public class ApiV1ReviewController {
 
   //리뷰 생성
   @PostMapping
-  public ApiResponse<ReviewCreateResponse> createReview(@RequestBody ReviewCreateRequest request
+  public ResponseEntity<ApiResponse<ReviewCreateResponse>> createReview(@RequestBody ReviewCreateRequest request
 //      , @AuthenticationPrincipal SecurityUser user
       //todo: 인증객체 받아와서 createReview에 전달
   ) {
     Long userId = 1L; //임시 유저 id
     ReviewCreateResponse response = reviewService.createReview(request, userId);
-    return ApiResponse.success(response);
+    return ResponseEntity.ok(ApiResponse.success(response));
+  }
+
+  //리뷰 삭제
+  @DeleteMapping("/{reviewId}")
+  public ResponseEntity<ApiResponse<String>> deleteReview(
+//      @AuthenticationPrincipal SecurityUser user,
+      //todo: 인증객체 받아와서 deleteReview에 전달
+      @PathVariable Long reviewId
+  ) {
+    Long userId = 1L; //임시 유저 id
+    reviewService.deleteReview(userId, reviewId);
+    return ResponseEntity.ok(ApiResponse.success(null));
   }
 }
