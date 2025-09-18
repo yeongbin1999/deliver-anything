2b8175232a8b235d372379be6df366647e8b3a2d
qkek123 <qkek622378@gmail.com>
2025-09-18 11:00:36 +0900

    test: test용 factory class 생성

--- backend/src/test/java/com/deliveranything/domain/review/controller/ApiV1ReviewControllerTest.java
+++ backend/src/test/java/com/deliveranything/domain/review/controller/ApiV1ReviewControllerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.deliveranything.domain.review.dto.ReviewCreateRequest;
 import com.deliveranything.domain.review.dto.ReviewCreateResponse;
 import com.deliveranything.domain.review.enums.ReviewTargetType;
+import com.deliveranything.domain.review.factory.ReviewFactory;
 import com.deliveranything.domain.review.repository.ReviewRepository;
 import com.deliveranything.domain.review.service.ReviewService;
 import com.deliveranything.domain.user.entity.User;
@@ -22,9 +23,6 @@ public class ApiV1ReviewControllerTest {
 
   @Autowired
   private ReviewService reviewService;
-
-  @Autowired
-  private ReviewRepository reviewRepository;
 
   @Autowired
   private UserRepository userRepository;
@@ -44,23 +42,16 @@ public class ApiV1ReviewControllerTest {
 
     userRepository.save(user);
 
-    ReviewCreateRequest request = new ReviewCreateRequest(
-        5,
-        "안녕하세요",
-        new String[]{"url1", "url2"},
-        ReviewTargetType.STORE,
-        1L
-    );
-
+    ReviewCreateRequest request = ReviewFactory.createReviews(1).getFirst();
 
     reviewService.createReview(request, userId);
 
     // 리뷰 등록 호출
     ReviewCreateResponse response = reviewService.createReview(request, userId);
 
-    // 간단 검증 (예: 리뷰 id가 생성됐는지)
+    // 검증
     assertNotNull(response.id());
-    assertEquals(5, response.rating());
-    assertEquals("안녕하세요", response.comment());
+    assertEquals(1, response.rating());
+    assertEquals("test comment1", response.comment());
   }
 }
