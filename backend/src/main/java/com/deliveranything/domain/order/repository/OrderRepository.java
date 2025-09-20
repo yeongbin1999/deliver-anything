3b1f34f9d702cfa6598a1555831f72c13e7c0745
flffkd12 <flffkd12@gmail.com>
2025-09-20 18:25:44 +0900

    feat(be): 소비자 전체 주문 조회 API 구현

--- backend/src/main/java/com/deliveranything/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/deliveranything/domain/order/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.deliveranything.domain.order.repository;
 
 import com.deliveranything.domain.order.entity.Order;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+  List<Order> findAllByCustomerId(Long customerId);
 }
