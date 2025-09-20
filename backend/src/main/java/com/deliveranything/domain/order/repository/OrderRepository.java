966cd6fb0e9c6cb8e020e2f09ecc397c77e8469e
DreamTree <166996451+flffkd12@users.noreply.github.com>
2025-09-20 20:05:31 +0900

    feat(be): 주문 단일 조회 API 구현 (#75)
    * feat(be): 소비자 단일 주문 조회 API 구현
    
    * feat(be): 소비자 주문 관련 에러 코드 추가
    
    * chore(be): 가독성 향상 및 에러 메시지 마침표와 같은 일관성 통일

--- backend/src/main/java/com/deliveranything/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/deliveranything/domain/order/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.deliveranything.domain.order.repository;
 
 import com.deliveranything.domain.order.entity.Order;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
   List<Order> findAllByCustomerId(Long customerId);
+  Optional<Order> findByIdAndCustomerId(Long orderId, Long customerId);
 }
