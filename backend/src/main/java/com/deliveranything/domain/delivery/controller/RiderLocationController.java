8d05fcea2b80d9e8c350a5526aa0373a38ffd6ed
Chaeruin <cms3604@naver.com>
2025-09-22 12:49:59 +0900

    feat: message send topic 설정

--- backend/src/main/java/com/deliveranything/domain/delivery/controller/RiderLocationController.java
+++ backend/src/main/java/com/deliveranything/domain/delivery/controller/RiderLocationController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -15,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RiderLocationController {
 
   private final RiderLocationService riderLocationService;
+  private final SimpMessagingTemplate messagingTemplate;
 
-  @MessageMapping("/location") // 클라이언트에서 /app/location 으로 전송
+  // 라이더 클라이언트에서 서버로 [[/app/location]] 으로 전송
+  // 서버에서 소비자 클라이언트로  [[/topic/location]] 으로 전송
+  @MessageMapping("/location")
   public void updateLocation(@Valid @Payload RiderLocationDto location) {
     riderLocationService.saveRiderLocation(location);
+    messagingTemplate.convertAndSend("/topic/location", location);
   }
 }
