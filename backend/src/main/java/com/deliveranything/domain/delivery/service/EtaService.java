37527ebc2eb64e4853386c394cacd8df5e484db9
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-09-24 11:00:57 +0900

    feat(be): 반경 내 라이더 검색 + ETA 계산 서비스 구현 (#110)
    * fix: conflict 해결 시 발생한 문법 오류 수정
    
    * feat: Redis GeoSearch 및 OSRM Table API 활용한 ETA 계산 비동기 Service 구현
    
    * chore: 예외 처리 필요 부분 주석 표기

---
+++ backend/src/main/java/com/deliveranything/domain/delivery/service/EtaService.java
@@ -0,0 +1,49 @@
+package com.deliveranything.domain.delivery.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.geo.Point;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class EtaService {
+
+  private final WebClient osrmWebClient;
+
+  // OSRM Table API 호출 (Reactive)
+  public Mono<Map<String, Double>> getEtaForMultipleReactive(
+      // double storeLat, double storeLon,
+      double userLat, double userLon,
+      List<Point> riderPoints,
+      List<String> riderIds
+  ) {
+    StringBuilder coordinates = new StringBuilder();
+    coordinates.append(userLon).append(",").append(userLat); // 0번 → 고객 좌표
+
+    for (Point p : riderPoints) {
+      coordinates.append(";").append(p.getX()).append(",").append(p.getY());
+    }
+
+    return osrmWebClient.get()
+        .uri("/table/v1/driving/{coordinates}?annotations=duration", coordinates)
+        .retrieve()
+        .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+        })
+        .map(response -> {
+          // 타입 캐스팅 예외 주의 및 처리 필요 -> 예정
+          List<List<Double>> durations = (List<List<Double>>) response.get("durations");
+          Map<String, Double> etaMap = new HashMap<>();
+          for (int i = 0; i < riderIds.size(); i++) {
+            double etaSec = durations.get(0).get(i + 1);
+            etaMap.put(riderIds.get(i), etaSec / 60.0); // 분 단위 변환
+          }
+          return etaMap;
+        });
+  }
+}
