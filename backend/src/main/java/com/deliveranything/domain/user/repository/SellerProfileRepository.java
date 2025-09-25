c9ad834657b560a3dd33a0d0dbba20381fd340a0
daran2 <129312040+daran2@users.noreply.github.com>
2025-09-25 17:23:06 +0900

    feat(be): SellerProfileService 구현 (#119)
    * feat(be): Store 도메인과 연동을 위한 StoreClient 생성
    
    * feat(be) : SellerProfileService - 프로필 생성하면서 Store도 생성되도록 연동
    
    * feat(be) : SellerProfileService 완성
    
    * refactor(be): Store 사양에 맞게 CustomerAddress의 위도 경도 변수를 Point 객체로 변경

--- backend/src/main/java/com/deliveranything/domain/user/repository/SellerProfileRepository.java
+++ backend/src/main/java/com/deliveranything/domain/user/repository/SellerProfileRepository.java
@@ -1,10 +1,19 @@
 package com.deliveranything.domain.user.repository;
 
 import com.deliveranything.domain.user.entity.profile.SellerProfile;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SellerProfileRepository extends JpaRepository<SellerProfile, Long> {
-  
+
+  boolean existsByBusinessCertificateNumber(String businessCertificateNumber);
+
+  Optional<SellerProfile> findByBusinessCertificateNumber(String businessCertificateNumber);
+
+  @Query("SELECT sp FROM SellerProfile sp WHERE sp.user.id = :userId")
+  Optional<SellerProfile> findByUserId(@Param("userId") Long userId);
 }
