5e75da0874d06499eeb59bec1c8b548a5c21d089
qkek123 <qkek622378@gmail.com>
2025-09-25 17:11:40 +0900

    fix: 오타 수정

--- backend/src/main/java/com/deliveranything/domain/review/controller/ApiV1ReviewController.java
+++ backend/src/main/java/com/deliveranything/domain/review/controller/ApiV1ReviewController.java
@@ -118,7 +118,7 @@ public class ApiV1ReviewController {
 
   @DeleteMapping("/{reviewId}/like")
   @Operation(summary = "리뷰 좋아요 취소", description = "로그인한 사용자가 특정 리뷰에 좋아요를 누른 상태에서 다시 좋아요를 누르면 취소됩니다.")
-  public ResponseEntity<ApiResponse<ReviewLikeResponse>> unikeReview(
+  public ResponseEntity<ApiResponse<ReviewLikeResponse>> unLikeReview(
       //      @AuthenticationPrincipal SecurityUser user,
       //todo: 인증객체 받아와서 전달
       @PathVariable Long reviewId
