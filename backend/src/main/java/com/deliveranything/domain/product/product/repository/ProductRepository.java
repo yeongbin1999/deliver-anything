1f68121ea097f6fc51e2d1d8e7bbd0f0a815bf21
yeongbin1999 <yeongbin1999@gmail.com>
2025-09-29 17:57:31 +0900

    feat(be): 이벤트 헨들링

--- backend/src/main/java/com/deliveranything/domain/product/product/repository/ProductRepository.java
+++ backend/src/main/java/com/deliveranything/domain/product/product/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.deliveranything.domain.product.product.repository;
 
 import com.deliveranything.domain.product.product.entity.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
+  List<Product> findAllByStoreId(Long storeId);
 }
