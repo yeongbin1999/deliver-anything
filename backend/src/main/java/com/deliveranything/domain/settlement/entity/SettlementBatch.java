b1bfc8ebc458afe2ca77896bca300b1ad27c6f00
DreamTree <166996451+flffkd12@users.noreply.github.com>
2025-10-01 15:12:20 +0900

    feat(be): 정산 조회 API 구현(리스트, 단건) (#199)
    * chore(be): 안쓰는 필드 제거
    
    * refactor(be): 최종 정산액 필드 추가
    
    * feat(be): 정산 조회 API 구현(리스트, 단건)

--- backend/src/main/java/com/deliveranything/domain/settlement/entity/SettlementBatch.java
+++ backend/src/main/java/com/deliveranything/domain/settlement/entity/SettlementBatch.java
@@ -10,8 +10,10 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "settlement_batches")
@@ -33,17 +35,22 @@ public class SettlementBatch extends BaseEntity {
   @Column(nullable = false, precision = 19, scale = 2)
   private BigDecimal totalPlatformFee;
 
+  @Column(nullable = false, precision = 19, scale = 2)
+  private BigDecimal settledAmount;
+
   @Column(nullable = false)
   private LocalDate settlementDate;
 
   @Builder
   public SettlementBatch(TargetType targetType, Long targetId, BigDecimal targetTotalAmount,
-      Integer transactionCount, BigDecimal totalPlatformFee, LocalDate settlementDate) {
+      Integer transactionCount, BigDecimal totalPlatformFee, BigDecimal settledAmount,
+      LocalDate settlementDate) {
     this.targetType = targetType;
     this.targetId = targetId;
     this.targetTotalAmount = targetTotalAmount;
     this.transactionCount = transactionCount;
     this.totalPlatformFee = totalPlatformFee;
+    this.settledAmount = settledAmount;
     this.settlementDate = settlementDate;
   }
 }
--- backend/src/main/java/com/deliveranything/domain/settlement/repository/SettlementBatchRepository.java
+++ backend/src/main/java/com/deliveranything/domain/settlement/repository/SettlementBatchRepository.java
@@ -1,8 +1,11 @@
 package com.deliveranything.domain.settlement.repository;
 
 import com.deliveranything.domain.settlement.entity.SettlementBatch;
+import com.deliveranything.domain.settlement.enums.TargetType;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SettlementBatchRepository extends JpaRepository<SettlementBatch, Long> {
 
+  List<SettlementBatch> findAllByTargetIdAndTargetType(Long targetId, TargetType targetType);
 }
