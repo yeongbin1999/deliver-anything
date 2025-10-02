a9cab363c1aa8b304bbbc8c9796e607f5d076e46
flffkd12 <flffkd12@gmail.com>
2025-10-02 14:50:30 +0900

    refactor(be): 판매자의 주문 관련 요청 구조 분리
    * 생성된 상태와 주문 대기 상태의 모호성을 해결하기 위한 새 주문 상태 추가

---
+++ backend/src/main/java/com/deliveranything/domain/order/controller/SellerOrderController.java
@@ -0,0 +1,61 @@
+package com.deliveranything.domain.order.controller;
+
+import com.deliveranything.domain.order.dto.OrderResponse;
+import com.deliveranything.domain.order.service.StoreOrderService;
+import com.deliveranything.global.common.ApiResponse;
+import com.deliveranything.global.common.CursorPageResponse;
+import com.deliveranything.global.security.auth.SecurityUser;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/seller/orders")
+@RestController
+public class SellerOrderController {
+
+  private final StoreOrderService storeOrderService;
+
+  @GetMapping("/{storeId}/history")
+  @Operation(summary = "주문 내역 조회", description = "판매자가 주문 이력을 요청한 경우")
+  @PreAuthorize("@profileSecurity.isSeller(#securityUser) and @storeSecurity.isOwner(#storeId,#securityUser)")
+  public ResponseEntity<ApiResponse<CursorPageResponse<OrderResponse>>> getOrdersHistory(
+      @AuthenticationPrincipal SecurityUser securityUser,
+      @PathVariable Long storeId,
+      @RequestParam(required = false) String nextPageToken,
+      @RequestParam(defaultValue = "10") int size
+  ) {
+    return ResponseEntity.ok().body(ApiResponse.success("상점의 전체 주문 내역 조회 성공",
+        storeOrderService.getStoreOrdersByCursor(storeId, nextPageToken, size)));
+  }
+
+  @GetMapping("/{storeId}/pending")
+  @Operation(summary = "주문 수락 대기 목록 조회", description = "판매자가 상점의 주문 수락 대기 목록을 요청한 경우")
+  @PreAuthorize("@profileSecurity.isSeller(#securityUser) and @storeSecurity.isOwner(#storeId,#securityUser)")
+  public ResponseEntity<ApiResponse<List<OrderResponse>>> getPendingOrders(
+      @AuthenticationPrincipal SecurityUser securityUser,
+      @PathVariable Long storeId
+  ) {
+    return ResponseEntity.ok().body(ApiResponse.success("상점의 주문 수락 대기 목록 조회 성공",
+        storeOrderService.getPendingOrders(storeId)));
+  }
+
+  @GetMapping("/{storeId}/accepted")
+  @Operation(summary = "주문 현황 목록 조회", description = "판매자가 상점의 주문 처리 중인 목록을 요청한 경우")
+  @PreAuthorize("@profileSecurity.isSeller(#securityUser) and @storeSecurity.isOwner(#storeId,#securityUser)")
+  public ResponseEntity<ApiResponse<List<OrderResponse>>> getAcceptedOrders(
+      @AuthenticationPrincipal SecurityUser securityUser,
+      @PathVariable Long storeId
+  ) {
+    return ResponseEntity.ok().body(ApiResponse.success("상점의 주문 현황 목록 조회 성공",
+        storeOrderService.getAcceptedOrders(storeId)));
+  }
+}
