ff0924a9964f98a452968c5928d99ff2f7e2f1f1
flffkd12 <flffkd12@gmail.com>
2025-10-05 20:17:44 +0900

    feat(be): 상점의 주문 거절 요청에 의한 이벤트 처리
    * 주문 거절 이벤트 결제 도메인 구독 처리
    * 결제 취소 성공 이벤트 주문 도메인 구독 처리

---
+++ backend/src/main/java/com/deliveranything/domain/payment/subscirber/OrderRejectedEventSubscriber.java
@@ -0,0 +1,50 @@
+package com.deliveranything.domain.payment.subscirber;
+
+import com.deliveranything.domain.order.event.OrderRejectedEvent;
+import com.deliveranything.domain.payment.service.PaymentService;
+import com.deliveranything.global.exception.CustomException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderRejectedEventSubscriber implements MessageListener {
+
+  private final ApplicationEventPublisher eventPublisher;
+  private final RedisMessageListenerContainer container;
+  private final ObjectMapper objectMapper;
+  private final PaymentService paymentService;
+
+  @PostConstruct
+  public void registerListener() {
+    container.addMessageListener(this, new ChannelTopic("order-rejected-event"));
+  }
+
+  @Override
+  public void onMessage(@NonNull Message message, byte[] pattern) {
+    OrderRejectedEvent event = null;
+    try {
+      event = objectMapper.readValue(new String(message.getBody()), OrderRejectedEvent.class);
+      paymentService.cancelPayment(event.merchantUid(), event.cancelReason());
+    } catch (CustomException e) {
+      if (event != null) {
+        log.warn("Payment cancel failed for order {}: {}", event.orderId(), e.getMessage());
+        // TODO 결제가 SSE에다가 상점의 개인 사정으로 주문 취소 되었다. 3일 내에 계좌에 직접 환불된다. 전달
+      } else {
+        log.error("Failed to get payment request message from Redis", e);
+      }
+    } catch (Exception e) {
+      log.error("Failed to process order rejected event from Redis", e);
+    }
+  }
+}
