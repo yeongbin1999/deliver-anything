6cffe6975ef11f7a4ebb6ad9d921e26fc9b6dd6e
Chaeruin <cms3604@naver.com>
2025-10-07 12:51:26 +0900

    refactor: 변경 사항에 맞게 리팩토링

--- backend/src/main/java/com/deliveranything/domain/delivery/event/event/sse/OrderAssignmentSsePublisher.java
+++ backend/src/main/java/com/deliveranything/domain/delivery/event/event/sse/OrderAssignmentSsePublisher.java
@@ -2,6 +2,7 @@ package com.deliveranything.domain.delivery.event.event.sse;
 
 import com.deliveranything.domain.delivery.event.dto.RiderNotificationDto;
 import com.deliveranything.domain.notification.service.NotificationService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,12 @@ public class OrderAssignmentSsePublisher {
   private final NotificationService notificationService;
 
   // 프로필 단위 전체 전송 (해당 프로필의 모든 emitter로 전송)
-  public void publish(RiderNotificationDto event) {
-    notificationService.sendToAll(Long.parseLong(event.riderId()), "RIDER_ASSIGNMENT_NOTIFICATION",
-        event);
+  public void publish(List<RiderNotificationDto> events) {
+    events.forEach(event ->
+        notificationService.sendToAll(Long.parseLong(event.riderId()),
+            "RIDER_ASSIGNMENT_NOTIFICATION", event)
+    );
+
   }
 
 }
--- backend/src/main/java/com/deliveranything/domain/delivery/handler/redis/OrderAcceptedRedisSubscriber.java
+++ backend/src/main/java/com/deliveranything/domain/delivery/handler/redis/OrderAcceptedRedisSubscriber.java
@@ -1,7 +1,8 @@
 package com.deliveranything.domain.delivery.handler.redis;
 
-import com.deliveranything.domain.delivery.event.dto.RiderNotificationDto;
 import com.deliveranything.domain.delivery.event.event.sse.OrderAssignmentSsePublisher;
+import com.deliveranything.domain.delivery.service.OrderNotificationService;
+import com.deliveranything.domain.order.event.OrderAcceptedEvent;
 import com.deliveranything.global.exception.CustomException;
 import com.deliveranything.global.exception.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,12 +19,13 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 // 로드 밸런싱 / 다중 인스턴스 환경 대응하여 Redis Pub/Sub 구축
-public class OrderAssignmentRedisSubscriber implements MessageListener {
+public class OrderAcceptedRedisSubscriber implements MessageListener {
 
-  private static final String CHANNEL = "order-assignment-events";
+  private static final String CHANNEL = "order-accepted-event";
   private final ObjectMapper objectMapper;
   private final RedisMessageListenerContainer container;
   private final OrderAssignmentSsePublisher orderAssignmentSsePublisher;
+  private final OrderNotificationService orderNotificationService;
 
   @PostConstruct
   public void subscribe() {
@@ -37,8 +39,14 @@ public class OrderAssignmentRedisSubscriber implements MessageListener {
       String body = new String(message.getBody());
 
       if (CHANNEL.equals(channel)) {
-        RiderNotificationDto event = objectMapper.readValue(body, RiderNotificationDto.class);
-        orderAssignmentSsePublisher.publish(event);
+        OrderAcceptedEvent event = objectMapper.readValue(body, OrderAcceptedEvent.class);
+        orderNotificationService.processOrderEvent(event)
+            .doOnNext(orderAssignmentSsePublisher::publish)
+            .doOnError(error -> {
+              log.error("Failed to process order event: {}", error.getMessage());
+            })
+            .subscribe();
+
       }
     } catch (Exception e) {
       throw new CustomException(ErrorCode.REDIS_MESSAGE_PROCESSING_ERROR);
